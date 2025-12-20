1b8d9b53427401ab84384f7642f35343f9c3cd9c
leerlandais <leerlandais@gmail.com>
2025-12-20 17:18:47 +0100

    started making my version of AppTime

--- model/Manager/AppTimeManager.php
+++ model/Manager/AppTimeManager.php
@@ -2,10 +2,20 @@
 
 namespace model\Manager;
 
+use DateTime;
 use model\Abstract\AbstractManager;
 
 class AppTimeManager extends AbstractManager
 {
-    public function getAppTime()
-    {}
+    public function getAppTime($amount = 0)
+    {
+        $seconds = $amount * 3600;
+        $time = new DateTime();
+        $time->modify('+' . $this->getTimeOffset() + $seconds);
+    }
+
+    private function getTimeOffset() : int
+    {
+        return $_SESSION["time_offset"] ?? 0;
+    }
 }
